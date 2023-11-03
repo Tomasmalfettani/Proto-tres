32557cd20059c9fa003720d75112e0ecd3a724af
Tomas <tomasmalfettani@outlok.com>
2023-11-03 14:29:43 -0300

    Cards servicios

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scrolled');
     }
 });
+
+// Agregar un controlador de eventos al botón "Más Información"
+document.querySelectorAll(".service-card button").forEach(function (button) {
+    button.addEventListener("click", function (e) {
+        e.preventDefault(); // Evita que el enlace redirija
+
+        // Encuentra la capa adicional en la tarjeta actual
+        const card = button.closest(".service-card");
+        const additionalInfoOverlay = card.querySelector(".additional-info-overlay");
+
+        // Muestra la capa adicional
+        additionalInfoOverlay.style.display = "block";
+    });
+});
+
+// Agregar un controlador de eventos al botón "Cerrar"
+document.querySelectorAll(".service-card #close-info").forEach(function (closeButton) {
+    closeButton.addEventListener("click", function () {
+        // Encuentra la capa adicional en la tarjeta actual
+        const card = closeButton.closest(".service-card");
+        const additionalInfoOverlay = card.querySelector(".additional-info-overlay");
+
+        // Oculta la capa adicional
+        additionalInfoOverlay.style.display = "none";
+    });
+});
